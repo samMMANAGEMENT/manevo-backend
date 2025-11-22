816ea29d01051ccd72a93e38362ab6841939e813
Samuel Pineda Lopez <samuel.pineda@sumimedical.com>
2025-11-22 18:44:45 -0500

    serve

--- routes/admin/servicios.php
+++ routes/admin/servicios.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('servicios')->group(function () {
     Route::post('crear-servicio', [ServiciosController::class, 'crearServicio']);
     Route::get('obtener-servicio', [ServiciosController::class, 'obtenerServicios']);
-    Route::put('modificar-servicio/{id}', [ServiciosController::class, 'modificarServicio']);
+    Route::put('modificar-servicio/{id}', [ServiciosController::class, 'modificarServicio']); 
 });
 
